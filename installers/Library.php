2e62a7122684bc83d6fde4884ef489ae6767bdd9
Nikolay Gorohovsky <fi1a@icloud.com>
2022-11-06 06:00:31 +0500

    Поддержка модуля fi1a/installers

---
+++ installers/Library.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Fi1a\Installers;
+
+/**
+ * Библиотека
+ */
+class Fi1aBitrixD7ModuleInstallerDemoLibrary extends AbstractLibrary
+{
+    /**
+     * @inheritDoc
+     */
+    public function canInstall(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function canUninstall(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function install(): bool
+    {
+        $this->output->writeln('<success>Install fi1a/bitrix-d7-module-installer-demo success</success>');
+
+        return true;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function uninstall(): bool
+    {
+        $this->output->writeln('<success>Uninstall fi1a/bitrix-d7-module-installer-demo success</success>');
+
+        return true;
+    }
+}
